928e5a604a851ed9b536dfcbaa1ab4291079d791
ZHAMoonlight <moonlight.xu@gmail.com>
2024-03-12 15:53:38 +0800

    调整RatioCompactPolicy参数，更容易理解
    Signed-off-by: ZHAMoonlight <moonlight.xu@gmail.com>

--- src/main/java/com/mt/hbase/chpt07/compact/RatioCompactPolicy.java
+++ src/main/java/com/mt/hbase/chpt07/compact/RatioCompactPolicy.java
@@ -10,11 +10,12 @@ import java.util.List;
  */
 public class RatioCompactPolicy {
 
-    public static List<Integer> applyCompactionPolicy(List<Integer> candidates , boolean mayBeStuck){
+    public static List<Integer> applyCompactionPolicy(List<Integer> candidates,
+        boolean mayBeStuck) {
         int minFileToCompact = 3;
-        int maxFileToCompact = 5;
-        int minCompactSize = 20;
-        double ratio = 0.1;
+        int maxFileToCompact = 10;
+        int minCompactSize = 100;
+        double ratio = 1.2;
         int start = 0;
         // get store file sizes for incremental compacting selection.
         final int countOfFiles = candidates.size();
@@ -25,16 +26,16 @@ public class RatioCompactPolicy {
             // calculate the sum of fileSizes[i,i+maxFilesToCompact-1) for algo
             int tooFar = i + maxFileToCompact - 1;
             sumSize[i] = fileSizes[i] + ((i + 1 < countOfFiles) ? sumSize[i + 1] : 0) - ((tooFar
-                    < countOfFiles) ? fileSizes[tooFar] : 0);
+                < countOfFiles) ? fileSizes[tooFar] : 0);
         }
 
         while (countOfFiles - start >= minFileToCompact &&
-                fileSizes[start] > Math.max(minCompactSize, (sumSize[start + 1] * ratio))) {
+            fileSizes[start] > Math.max(minCompactSize, (sumSize[start + 1] * ratio))) {
             ++start;
         }
         if (start < countOfFiles) {
             System.out.println("Default compaction algorithm has selected " + (countOfFiles - start)
-                    + " files from " + countOfFiles + " candidates");
+                + " files from " + countOfFiles + " candidates");
         } else if (mayBeStuck) {
             // We may be stuck. Compact the latest files if we can.
             int filesToLeave = candidates.size() - minFileToCompact;
@@ -46,24 +47,16 @@ public class RatioCompactPolicy {
         System.out.println("start=" + start);
         candidates.subList(0, start).clear();
         System.out.println("fileToCompact =" + candidates);
-        /**
-         * 输出结果如下：
-         * Default compaction algorithm has selected 3 files from 7 candidates
-         * sumSize=[150, 120, 90, 65, 35, 15, 5]
-         * start=4
-         * fileToCompact =[20, 10, 5]
-         */
         return candidates;
     }
 
     public static void main(String[] args) {
-        int[] candidateIntArray = new int[] { 50, 40, 30, 30, 20, 10, 5 };
+        int[] candidateIntArray = new int[]{8200, 4300, 160, 150, 80, 70, 30, 20, 5};
         List<Integer> candidateList = new LinkedList<Integer>();
-        for(Integer candidate : candidateIntArray){
+        for (Integer candidate : candidateIntArray) {
             candidateList.add(candidate);
         }
-        applyCompactionPolicy(candidateList,true);
-
+        applyCompactionPolicy(candidateList, true);
     }
 
 }
