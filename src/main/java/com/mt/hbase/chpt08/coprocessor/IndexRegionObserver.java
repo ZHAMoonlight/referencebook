37d21efcd0fa2bedf1805d5fd8719c92d4df9dfa
ZHAMoonlight <moonlight.xu@gmail.com>
2024-04-24 10:39:17 +0800

    修改表名
    Signed-off-by: ZHAMoonlight <moonlight.xu@gmail.com>

--- src/main/java/com/mt/hbase/chpt08/coprocessor/IndexRegionObserver.java
+++ src/main/java/com/mt/hbase/chpt08/coprocessor/IndexRegionObserver.java
@@ -15,11 +15,11 @@ import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.hbase.wal.WALEdit;
 
 /**
- * 该Observer的作用是在s_order表写入的时候， 抽取refund_no、行键写入i_order_refund表建立二级索引
+ * 该Observer的作用是在s_contact表写入的时候， 抽取name、行键写入i_contact_name表建立二级索引
  */
 public class IndexRegionObserver implements RegionCoprocessor, RegionObserver {
-    private static final String REFUND_NO = "refund_no";
-    private static final String INDEX_TABLE_NAME = "i_order_refund";
+    private static final String NAME = "name";
+    private static final String INDEX_TABLE_NAME = "i_contact_name";
     private static final String CF = "cf";
     private static final String COLUMN_K = "k";
     @Override
@@ -30,18 +30,17 @@ public class IndexRegionObserver implements RegionCoprocessor, RegionObserver {
     public void prePut(ObserverContext<RegionCoprocessorEnvironment> c, Put put, WALEdit edit)
         throws IOException {
         RegionObserver.super.prePut(c, put, edit);
-
-        // 当一行数据中带有退款单号才写索引
-        List<Cell> putRefundCells = put.get(Bytes.toBytes(CF), Bytes.toBytes(REFUND_NO));
-        if(putRefundCells==null || putRefundCells.size()==0){
+        // 当一行数据中带有name才写索引
+        List<Cell> putNameCells = put.get(Bytes.toBytes(CF), Bytes.toBytes(NAME));
+        if(putNameCells==null || putNameCells.size()==0){
             return;
         }
-        // 获取refund_no和行键
-        byte[] refundNo = putRefundCells.get(0).getValueArray();
+        // 获取name和行键
+        byte[] name = putNameCells.get(0).getValueArray();
         byte[] rowKey = put.getRow();
 
         RegionCoprocessorEnvironment env = c.getEnvironment();
-        Put indexPut = new Put(Bytes.toBytes(Bytes.toString(refundNo)));
+        Put indexPut = new Put(Bytes.toBytes(Bytes.toString(name)));
         indexPut.addColumn(Bytes.toBytes(CF), Bytes.toBytes(COLUMN_K), rowKey);
         Table indexTable = env.getConnection().getTable(TableName.valueOf(INDEX_TABLE_NAME));
         indexTable.put(indexPut);
